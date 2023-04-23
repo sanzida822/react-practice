93f01fa31f3e85b3c48c75ce881e318279c50de7
unknown <sanzida822>
2023-04-24 02:32:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Header from './Header/Header'
+
+function App() {
+
+
+  return (
+  <Header className="">
+    
+  </Header>
+     
+  )
+}
+
+export default App
---
+++ src/Header/Header.jsx
@@ -0,0 +1,19 @@
+
+import './Header.css'
+import logo from '../../images/Logo.svg'
+const Header = () => {
+    return (
+        <nav className='header'>
+            <div><img src={logo} alt="" /></div>
+
+            <div>
+                <a href="shop">shop</a>
+                <a href="order">order</a>
+                <a href="/inventory">inventory</a>
+                <a href="/login">login</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
